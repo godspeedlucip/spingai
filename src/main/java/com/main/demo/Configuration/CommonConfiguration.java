49a8611023483945ecf9dc42b8fbf5fc5ed62aa0
godspeedlucip <1959962049@qq.com>
2026-03-04 21:39:07 +0800

    RAG功能

--- src/main/java/com/main/demo/Configuration/CommonConfiguration.java
+++ src/main/java/com/main/demo/Configuration/CommonConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.ollama.OllamaChatModel;
@@ -25,23 +26,28 @@ import org.springframework.context.annotation.Configuration;
 public class CommonConfiguration {
 
     @Autowired
-    private ChatMemoryRepository chatMemoryRepository; //默认是InMemoryRepository
-
-    @Autowired
     private CourseTools courseTools;
 
-    @Bean
-    public ChatMemory inmemory_chatMemory(ChatMemoryRepository repo) {
+    // 显式创建一个基于内存的ChatMemoryRepository实例，并注册为Bean，这样Spring就会使用这个实例，而不是自动配置的JdbcChatMemoryRepository
+    @Bean(name="inMemoryChatMemoryRepository")
+    public InMemoryChatMemoryRepository inMemoryChatMemoryRepository() {
+        return new InMemoryChatMemoryRepository();
+    }
+
+    // chat memory部分
+    @Bean(name="inmemory_chatMemory")
+    public ChatMemory inmemory_chatMemory(@Qualifier("inMemoryChatMemoryRepository") InMemoryChatMemoryRepository inMemoryChatMemoryRepository) {
+        // 这里的参数会自动匹配到bean
         return MessageWindowChatMemory.builder()
-                .chatMemoryRepository(repo)
-                .maxMessages(50) // 最多保留 50 条
+                .chatMemoryRepository(inMemoryChatMemoryRepository)
+                .maxMessages(200) // 最多保留 50 条
                 .build();
     }
 
-    @Bean
-    public ChatMemory jdbc_chatMemory(JdbcChatMemoryRepository chatMemoryRepository) {
+    @Bean(name="jdbc_chatMemory")
+    public ChatMemory jdbc_chatMemory(@Qualifier("jdbcChatMemoryRepository") JdbcChatMemoryRepository jdbcChatMemoryRepository) {
         return MessageWindowChatMemory.builder()
-                .chatMemoryRepository(chatMemoryRepository)
+                .chatMemoryRepository(jdbcChatMemoryRepository)
                 .maxMessages(200)
                 .build();
     }
@@ -52,52 +58,55 @@ public class CommonConfiguration {
         return SimpleVectorStore.builder(openAiEmbeddingModel).build();
     }
 
-    @Bean
-    public ChatClient open_ai_chatClient(OpenAiChatModel model) {
+    // "chat"下的client
+    @Bean(name="open_ai_chatClient")
+    public ChatClient open_ai_chatClient(OpenAiChatModel model, @Qualifier("jdbc_chatMemory")ChatMemory memory) {
         return ChatClient.builder(model)
                 .defaultSystem("你是一个智能客服，用简洁的语气说话")
 //                .defaultSystem("你是一个可爱的萌妹子，用可爱的语气说话")
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
-                        MessageChatMemoryAdvisor.builder(inmemory_chatMemory(chatMemoryRepository)).build()
+                        MessageChatMemoryAdvisor.builder(memory).build()
                 )
                 .build();
     }
 
-    @Bean
-    public ChatClient ollama_chatClient(OllamaChatModel model) {
+    @Bean(name="ollama_chatClient")
+    public ChatClient ollama_chatClient(OllamaChatModel model, @Qualifier("jdbc_chatMemory") ChatMemory memory) {
         return ChatClient.builder(model)
                 .defaultSystem("你是一个智能客服，用简洁的语气说话")
 //                .defaultSystem("你是一个可爱的萌妹子，用可爱的语气说话")
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
-                        MessageChatMemoryAdvisor.builder(inmemory_chatMemory(chatMemoryRepository)).build()
+                        MessageChatMemoryAdvisor.builder(memory).build()
                 )
                 .build();
     }
 
-    @Bean
-    public ChatClient service_chatClient(OpenAiChatModel model) {
+    // "service"下的client，增加一些工具
+    @Bean(name="service_chatClient")
+    public ChatClient service_chatClient(OpenAiChatModel model, @Qualifier("inmemory_chatMemory") ChatMemory memory) {
         return ChatClient.builder(model)
                 .defaultSystem(SystemConstants.SERVICE_SYSTEM_PROMPT)
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
-                        MessageChatMemoryAdvisor.builder(inmemory_chatMemory(chatMemoryRepository)).build()
+                        MessageChatMemoryAdvisor.builder(memory).build()
                 )
                 .defaultTools(courseTools)
                 .build();
     }
 
-    @Bean
+    // "pdf"下的client，增加向量检索工具
+    @Bean(name="pdfChatClient")
     public ChatClient pdfChatClient(
             OpenAiChatModel model,
-            @Qualifier("jdbc_chatMemory") ChatMemory jdbc_chatMemory,
+            @Qualifier("jdbc_chatMemory") ChatMemory memory,
             VectorStore vectorStore) {
         return ChatClient.builder(model)
                 .defaultSystem("根据上下文内容来回答用户的问题，如果无法从上下文中找到答案，请说不知道，不要编造答案")
                 .defaultAdvisors(
                         SimpleLoggerAdvisor.builder().build(),
-                        MessageChatMemoryAdvisor.builder(jdbc_chatMemory).build(),
+                        MessageChatMemoryAdvisor.builder(memory).build(),
                         QuestionAnswerAdvisor
                                 .builder(vectorStore)
                                 .searchRequest(
